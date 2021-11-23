cbaff851d91b942fcd2ef3b791aaa75d4dd33375
Silverstorm-App-dev <appdev@silver-storm.com>
2021-11-23 12:29:17 +0100

    correccion de error location ../index.php

--- php/mail.php
+++ php/mail.php
@@ -36,7 +36,7 @@
     
         $mail->send();
         echo 'Message has been sent';
-        header("../index.php");
+        header("Location: ../index.php");
         exit();
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
