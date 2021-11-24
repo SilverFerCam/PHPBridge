e8526c1af49393eb4891d93ef9cd5fa4cf23d1f5
SilverFerCam <dcambronel@silver-storm.com>
2021-11-24 11:49:15 +0100

    segundo intento por solucionar el update del PDF

--- js/pdf.js
+++ js/pdf.js
@@ -233,18 +233,18 @@ document.getElementById("pdf").addEventListener("click",function(){
             var formData = new FormData();
             formData.append('pdf', blob);
 
-            $.ajax({
+            /*$.ajax({
                url: 'php/delete_file.php',
                data: {'file' :  'uploads/datosCuestionario.pdf'},
-               method: 'GET',
+               method: 'GET',/*
                /*success: function (response) {
                   alert('Deleted!');
      
                },
                error: function () {
                   alert('Not Deleted!');
-               }*/
-            });
+               }*//*
+            });*/
 
             $.ajax('php/upload.php',
             {
--- php/upload.php
+++ php/upload.php
@@ -1,4 +1,5 @@
 <?php
+    unlink("../uploads/datosCuestionario.pdf");
     move_uploaded_file(
         $_FILES['pdf']['tmp_name'], 
         "../uploads/datosCuestionario.pdf"
